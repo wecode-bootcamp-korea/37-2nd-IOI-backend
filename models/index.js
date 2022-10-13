3062efaa817fcd77ab3440dc5856d8bd257f657e
SR-Jung <westeeven0612@gmail.com>
2022-10-13 16:25:15 +0900

    conflict

--- models/index.js
+++ models/index.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 const userDao = require("./userDao")
-
-module.exports = {
-    userDao
-=======
 const appDataSource = require('./dataSource')
 const pageFiltersDao = require('./pageFiltersDao')
 
 module.exports = {
+    userDao,
     appDataSource,
     pageFiltersDao,
->>>>>>> 1dbb4b6 (GET 메인페이지 정렬 리팩토링 - 가격 높은순, 낮은순, 좋아요 높은순)
 }
